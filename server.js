75eccdd1ed0a28b7c4502df955fdaaf041688ed2
erik.minasian <eminasian@webmd.net>
2025-06-28 13:56:05 +0400

    feat: add item routes

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 const fastify = require("fastify")({logger: true})
+const items = require("./mocks/items.json")
 
 fastify.get('/health', (req, reply) => { 
   reply.send('Hello World!')
+})
+
+fastify.get('/items', (req, reply) => {
+  reply.send(items)
+})
+
+fastify.get('/item/:id', (req, reply) => {
+  const { id } = req.params
+  const item = items.find(item => item.id === Number(id))
+  reply.send(item)
 })
 
 const start = async() => {
